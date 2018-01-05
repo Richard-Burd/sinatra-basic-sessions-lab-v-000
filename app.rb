dca1803d3a8a5fc172f7483d0f8828e1e3877167
Richard Burd <richardburdx@yahoo.com>
2018-01-05 20:14:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-#   set :session_secret, "Barzalshatuach"
-    set :sod_biyoter, "maummah" # this can be anything for now
+    set :session_secret, "Barzalshatuach"
+    set :sodz_biyoter, "maumzmah" # this can be anything for now
   end
 
   get '/' do
@@ -15,7 +15,30 @@ class App < Sinatra::Base
   post '/checkout' do
     session[:debar] = params[:debar]
     @cessissioughn = session  # the instance variable can be named anything
+    #binding.pry
     erb :checkout
   end
 
+  get '/newindex' do
+    erb :newindex
+  end
+
+  post '/mynewpost' do
+    session[:myitem] = params[:myitem]
+    @instancevar = session
+    if session[:myitem] == "Zaza"
+      redirect '/secret_chamber'
+    else
+    #binding.pry
+      erb :mynewpost
+    end
+  end
+
+  get '/secret_chamber' do
+    if session[:myitem] == "Zaza"
+      "Welcome to the Secret Chamber, you entered the password '#{session[:myitem]}'"
+    else
+      "ACCESS DENIED! You don't know the secret word."
+    end 
+  end
 end
--- spec/sinatra_basic_sessions_lab_spec.rb
+++ spec/sinatra_basic_sessions_lab_spec.rb
@@ -34,5 +34,43 @@ describe App do
     end
   end
 
+#///////////////////////////////////////////////////
+# THese are tests I made just to play around with stuff & functionality
+  describe 'Second GET /' do
+
+    it 'sends a 200 status code' do
+      get '/newindex'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'renders a form to purchase an item' do
+      visit '/newindex'
+      expect(page).to have_selector("form")
+      expect(page).to have_field(:myitem)
+    end
+  end
+
+  describe 'POST /mynewpost' do
+
+    it "displays some sort of thiny" do
+      visit '/newindex'
+
+      fill_in(:myitem, :with => "socks")
+      click_button "submit"
+      expect(page).to have_text("socks")
+    end
+
+    it "does something else too" do
+      # let's rename "params" to "arguments" just to see what breaks
+      # or what we can seriously fuck up
+      arguments = {
+        :myitem => "socks"
+      }
+      post '/mynewpost', arguments
+      expect(session.has_key?(:myitem)).to eq(true)
+      expect(session[:myitem]).to eq(arguments[:myitem])
+    end
+  end
+
 
 end
